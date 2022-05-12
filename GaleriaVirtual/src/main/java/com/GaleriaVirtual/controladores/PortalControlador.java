9a300fe419162e2e4976ea306f4530b17a98a9d7
BeluGarcia07 <belu.garcia07@gmail.com>
2022-05-12 19:23:33 -0300

    correccionSeguridad

--- GaleriaVirtual/src/main/java/com/GaleriaVirtual/controladores/PortalControlador.java
+++ GaleriaVirtual/src/main/java/com/GaleriaVirtual/controladores/PortalControlador.java
@@ -5,7 +5,9 @@ import com.GaleriaVirtual.servicios.UsuarioServicio;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,56 +16,57 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/")
 public class PortalControlador {
-    
+
     @Autowired
     private UsuarioServicio usuarioServicio;
-            
+
     @GetMapping("/")
     public String index() {
         return "index.html";
     }
-    
-     @GetMapping("/login")
-    public String login() {
-        return "login.html";      
-    } 
-    
+
     @GetMapping("/registro")
     public String registro() {
-        return "registro.html";  
+        return "registro.html";
     }
-    
+
     @PostMapping("/registrar")
-    public String registrar(@RequestParam String nickname, @RequestParam String mail, @RequestParam String contrasenia1,@RequestParam String contrasenia2){
-        
+    public String registrar(ModelMap modelo, @RequestParam String nickname, @RequestParam String mail, @RequestParam String contrasenia1, @RequestParam String contrasenia2) {
+
         try {
-            usuarioServicio.registrar(nickname, mail, contrasenia1);
+            usuarioServicio.registrar(nickname, mail, contrasenia1, contrasenia2);
         } catch (ErrorServicio ex) {
-            Logger.getLogger(PortalControlador.class.getName()).log(Level.SEVERE, null, ex);
+            modelo.put("errorReg", ex.getMessage());
+            modelo.put("nickname", nickname);
+            modelo.put("mail", mail);
+            modelo.put("contrasenia1", contrasenia1);
+            modelo.put("contrasenia2", contrasenia2);
             return "registro.html";
         }
-        return "login.html";       
-        
+        modelo.put("titulo", "Bienvenido a la Galería de Arte Tamago!");
+        modelo.put("descripcion", "Logeate para comenzar");
+        return "/";
+
     }
+
+    @GetMapping("/login")
+    public String login(@RequestParam(required = false) String error, ModelMap model) {
+        if (error != null) {
+            model.put("error", "Usuario o contraseña incorrecta  .");
+        }
+        return "login.html";
+    }
+
+//    @PreAuthorize("hasAnyRol('ROL_USER_REGISTRADO')")
+//    @GetMapping("/comprar")
+//    public String comprar() {
+//        return "comprar.html";
+//    }
+//    
+//    @PreAuthorize("hasAnyRol('ROL_USER_REGISTRADO')")
+//    @GetMapping("/vender")
+//    public String vender() {
+//        return "vender.html";
+//    }
+    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
